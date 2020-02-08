2acb88fb9313d56421dda5a25501403830602843
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2020-02-08 13:58:52 -0500

    Move All to SRC

---
+++ src/River.SourceInstaller/Program.cs
@@ -0,0 +1,15 @@
+﻿
+[assembly: TinyInstaller.InstallerIdentity("River Source Service")]
+[assembly: TinyInstaller.InstallUserMode(false)]
+[assembly: TinyInstaller.InstallUtilsAssembly(assembly: typeof(River.SourceService.Service))]
+
+namespace River.MouthInstaller
+{
+	class Program
+	{
+		static void Main()
+		{
+			TinyInstaller.EntryPoint.GuiRunWith("River Source Service");
+		}
+	}
+}
