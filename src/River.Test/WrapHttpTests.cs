2acb2684c6f905bedf444d4117af88821c45b0a5
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2020-03-08 22:27:29 -0400

    add http wrap multiplied tests

--- src/River.Test/WrapHttpTests.cs
+++ src/River.Test/WrapHttpTests.cs
@@ -25,5 +25,34 @@ namespace River.Test
 			cli.Dispose();
 			proxy.Dispose();
 		}
+
+		[TestMethod]
+		public void Should_wrap_http_mult_dispose()
+		{
+			for (var i = 0; i < 10; i++)
+			{
+				var port = GetFreePort();
+				var proxy = new HttpWrapServer("hw://chacha20:123test@0.0.0.0:" + port);
+
+				var cli = new HttpWrapClientStream("chacha20", "123test", "localhost", port, Host, 80);
+				TestConnction(cli, Host);
+
+				cli.Dispose();
+				proxy.Dispose();
+			}
+		}
+
+		[TestMethod]
+		public void Should_wrap_http_mult_track()
+		{
+			for (var i = 0; i < 10; i++)
+			{
+				var port = GetFreePort();
+				var proxy = new HttpWrapServer("hw://chacha20:123test@0.0.0.0:" + port).Track();
+
+				var cli = new HttpWrapClientStream("chacha20", "123test", "localhost", port, Host, 80).Track();
+				TestConnction(cli, Host);
+			}
+		}
 	}
 }
